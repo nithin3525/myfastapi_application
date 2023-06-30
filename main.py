c7c303351c10a087c052a1b50b5b89475c2b8709
nithin3525 <133799862+nithin3525@users.noreply.github.com>
2023-06-30 23:11:38 +0530

    modified

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-@app.get("/admin")
-def admin():
-    return "hello admin!"
+@app.get("/blog")
+#we can give default values to the limit and bool
+#we can pass any no.of parameters and assign them 
+#optional
+#for that import optioanl from typing
+def user(limit = 10,published:bool=True,sort: Optional[str] = None):
+    if published:
+        return {"data":f"{limit} published blogs from db"}
+    else:
+        return {"data":f"{limit} blogs from db"}
 
+     
+@app.get("/blog/{blog_id}")
+def data(blog_id:int):
+    return {"data":blog_id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+
+    return {'data':[1,2,3]}
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data':{f'blog is created with title as {request.title}'}}
